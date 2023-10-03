84d21db9074536434930b8fb4b6fb979a77907a4
mherrafiq <mher.rafiq@gmail.com>
2023-10-03 12:41:24 +0500

    trying to set next project in node hirarchy

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require("express");
+const next = require("next");
 const session = require('express-session');
 const passport = require('./oauth');
 const path = require('path'); // Import the path module
 
+// const app = next({ dev });
 const app = express();
-
 const UserRoute = require('./routes/Route');
 // app.use('/user', UserRoute);\
+
+
+//nextapp
+const dev = process.env.NODE_ENV !== 'production';
+const appNext = next({ dev });
+const handle = appNext.getRequestHandler();
+
+appNext.prepare().then(() => {
+    app.get('*', (req, res) => {
+      return handle(req, res);
+    });
+  
+    app.listen(port, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${port}`);
+    });
+  });
+  return 1;
 
 function isLoggedin(req, res, next){//middleware
     req.user ? next() : res.sendStatus(401);
@@ -14,8 +33,12 @@ function isLoggedin(req, res, next){//middleware
 
 app.use(passport.initialize());
 app.get('/', (req, res) =>{
-    const filePath = path.join(__dirname, 'index.html');
-    res.sendFile(filePath);
+    console.log(__dirname)
+    const filePath = path.join(__dirname, '/views/next.js');
+    res.send(filePath)
+    // const filePath = path.join(__dirname, 'index.html');
+    // res.sendFile(filePath);
+    // res.render(filePath);
 });
 
 app.use(session({
